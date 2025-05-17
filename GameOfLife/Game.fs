36240f041e72e769432c8ba1f301305de9b88858
krymtkts <takatoshi.kuriyama@gmail.com>
2025-05-17 12:31:39 +0900

    Refactor neighbor calculations for clarity and simplify.

--- GameOfLife/Game.fs
+++ GameOfLife/Game.fs
@@ -22,15 +22,18 @@ let inline isCellLive cell =
     | Dead -> 0
     | Live -> 1
 
-let inline liveAt grid (x, y) =
-    let index = toIndex grid x y
+let inline (|OutOfRange|_|) grid (x, y) =
+    x < 0
+    || x >= grid.width
+    || y < 0
+    || y >= grid.height
 
-    match index with
-    | z when x >= grid.width -> Dead
-    | z when x < 0 -> Dead
-    | z when y < 0 -> Dead
-    | z when y >= grid.height -> Dead
-    | z -> List.item index grid.Grid
+let inline liveAt grid (x, y) =
+    match x, y with
+    | OutOfRange grid -> Dead
+    | _ ->
+        let index = toIndex grid x y
+        List.item index grid.Grid
 
 let inline calcNeighborsRange game index =
     let (x, y) = toXY game index
@@ -44,14 +47,18 @@ let inline calcNeighborsRange game index =
       x, y - 1
       x + 1, y - 1 ]
 
+let inline (|Survive|_|) (cell: Cell, lives) = cell.IsLive && (lives = 2 || lives = 3)
+let inline (|Birth|_|) (cell: Cell, lives) = cell.IsDead && lives = 3
+
 let inline cycleElement game index element =
-    let neighborRange = calcNeighborsRange game index
-    let neighbors = List.mapi (fun x y -> liveAt game y) neighborRange
-    let neighborsLive = List.sumBy (fun x -> isCellLive x) neighbors
+    let neighborsLive =
+        calcNeighborsRange game index
+        |> List.map (fun neighbor -> liveAt game neighbor)
+        |> List.sumBy (fun x -> isCellLive x)
 
     match element, neighborsLive with
-    | Live, x when element = Live && (x = 2 || x = 3) -> Live
-    | Dead, x when element = Dead && x = 3 -> Live
+    | Birth
+    | Survive -> Live
     | _ -> Dead
 
 let inline Cycle game =

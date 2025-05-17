2de2da9516a0991edd4bfdba8ddbc722c66ce1a5
krymtkts <takatoshi.kuriyama@gmail.com>
2025-05-17 11:34:56 +0900

    Apply formatting.

--- GameOfLife/Program.fs
+++ GameOfLife/Program.fs
@@ -3,7 +3,7 @@ open System.Threading
 open GameOfLife.Game
 open System.IO
 
-let writer = 
+let writer =
     let sw = new StreamWriter(Console.OpenStandardOutput())
     sw.AutoFlush <- false
     sw |> Console.SetOut
@@ -13,16 +13,25 @@ let inline AliveCount grid =
     List.sumBy (fun x -> if x = Live then 1 else 0) grid
 
 let inline printGameState game loop =
-    let symbol cell = match cell with
-                        | Live -> "X"
-                        | Dead -> " "
+    let symbol cell =
+        match cell with
+        | Live -> "X"
+        | Dead -> " "
+
     let alive = AliveCount game.Grid
-    $"#Loop: {loop}        Living: {alive}" |> Console.WriteLine
+
+    $"#Loop: {loop}        Living: {alive}"
+    |> Console.WriteLine
+
     "" |> Console.WriteLine
-    List.iteri (fun index cell ->
-        match index % game.width with
-        | a when a+1 = game.width ->  symbol cell |> Console.WriteLine
-        | _ ->  symbol cell |> Console.Write) game.Grid
+
+    List.iteri
+        (fun index cell ->
+            match index % game.width with
+            | a when a + 1 = game.width -> symbol cell |> Console.WriteLine
+            | _ -> symbol cell |> Console.Write)
+        game.Grid
+
     writer.Flush()
 
 let rec GameLoop game loopNumber =
@@ -32,11 +41,16 @@ let rec GameLoop game loopNumber =
 
     match AliveCount game.Grid with
     | 0 -> "Game Over" |> Console.Write
-    | _ -> game |> Cycle |> GameLoop <| 1+loopNumber
+    | _ -> game |> Cycle |> GameLoop <| 1 + loopNumber
 
 let MakeGameBoard height width =
     let random = Random()
-    List.init (height * width) (fun index -> if random.NextDouble() > 0.41 then Live else Dead)
+
+    List.init (height * width) (fun index ->
+        if random.NextDouble() > 0.41 then
+            Live
+        else
+            Dead)
 
 [<EntryPoint>]
 let main argv =
@@ -44,6 +58,10 @@ let main argv =
     let height = Array.get argv 0 |> Int32.Parse
     let width = Array.get argv 1 |> Int32.Parse
 
-    let initialGameState = {Grid = MakeGameBoard height width; height = height; width = width}
+    let initialGameState =
+        { Grid = MakeGameBoard height width
+          height = height
+          width = width }
+
     GameLoop initialGameState 0
     0 // return an integer exit code
